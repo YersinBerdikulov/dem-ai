4785d875ff5ada12c7ae00d2405c4d725bf86e38
Yersultan Zharylkasyn <151725506+Yers001@users.noreply.github.com>
2025-06-10 09:58:36 +0600

    some isse

--- android/app/src/main/java/com/demai/app/MainActivity.kt
+++ android/app/src/main/java/com/demai/app/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : ReactActivity() {
     // Set the theme to AppTheme BEFORE onCreate to support
     // coloring the background, status bar, and navigation bar.
     // This is required for expo-splash-screen.
-    setTheme(R.style.AppTheme);
+    setTheme(com.demai.app.R.style.AppTheme);
     super.onCreate(null)
   }
 
@@ -32,7 +32,7 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
           this,
-          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          com.demai.app.BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
           object : DefaultReactActivityDelegate(
               this,
               mainComponentName,
